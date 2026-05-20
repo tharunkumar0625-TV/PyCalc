215b57682184bf679907b803bbe738f1973b68c4
tharunkumar0625-TV <tharunkumar0625@gmail.com>
2026-05-20 12:05:25 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,17 @@
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-operation = input("Enter the operation (+, -, *, /): ")
-if operation == "+":
+while True:
+ a = float(input("Enter the first number: "))
+ b = float(input("Enter the second number: "))
+ operation = input("Enter the operation (+, -, *, /): ")
+ if operation == "+":
     result = a + b
-elif operation == "-":
+ elif operation == "-":
     result = a - b
-elif operation == "*":
+ elif operation == "*":
     result = a * b
-elif operation == "/":
+ elif operation == "/":
     result = a / b
-else:
- result = None
-print(f"the result is {result:.2f}")
+ else:
+  result = None
+ print(f"the result is {result:.2f}")
+
+
